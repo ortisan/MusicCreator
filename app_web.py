1b00659713e0a27f04d23b45cdb7f311b7265924
Marcelo Ortiz De Santana <marcelo@MBP-de-Marcelo.lan>
2016-04-03 00:13:22 -0300

    Randomizacao e remocao da cadencia quando o cliente gosta ou desgosta.

--- app_web.py
+++ app_web.py
@@ -1,13 +1,17 @@
 from flask import Flask, url_for, render_template, redirect
 
 app = Flask(__name__, static_folder='cadences')
+import os
+import csv
+from random import shuffle
 
 
 class MIDIItem(object):
-    def __init__(self, name, midi_link, liked_link):
+    def __init__(self, name, midi_link, like_link, dislike_link):
         self.name = name
         self.midi_link = midi_link
-        self.liked_link = liked_link
+        self.like_link = like_link
+        self.dislike_link = dislike_link
 
 
 @app.route('/')
@@ -17,20 +21,40 @@ def index():
     items = []
     for f in listdir('cadences'):
         if isfile(join('cadences', f)) and f.endswith('.midi'):
-            midi_item = MIDIItem(f, url_for('static', filename=f), ('/liked/%s' % f))
+            midi_item = MIDIItem(f, url_for('static', filename=f), ('/like/%s' % f), ('/dislike/%s' % f))
             items.append(midi_item)
+
+    shuffle(items)
+
     return render_template('home.html', midi_items=items)
 
 
-@app.route('/liked/<name>')
+@app.route('/like/<name>')
 def like(name):
-    import csv
-    with open('liked_midis.csv', 'a') as csvfile:
+    with open('like_midis.csv', 'a') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=' ',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         spamwriter.writerow([name])
 
+    remove_midi('cadences/{0}'.format(name))
+
     return redirect('/')
+
+
+@app.route('/dislike/<name>')
+def dislike(name):
+    with open('dislike_midis.csv', 'a') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow([name])
+
+    remove_midi('cadences/{0}'.format(name))
+
+    return redirect('/')
+
+
+def remove_midi(file):
+    os.remove(file)
 
 
 if __name__ == '__main__':
